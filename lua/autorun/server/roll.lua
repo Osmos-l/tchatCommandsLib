725ae27ff1c98b964a366e4706485fd6de94b1e9
Osmos <emailjeux81@gmail.com>
2021-03-02 09:58:12 +0100

    Correct error: File is loaded before DarkRP

--- lua/autorun/server/roll.lua
+++ lua/autorun/server/roll.lua
@@ -1,37 +1,35 @@
-if ( DarkRP ) then
-    local random = math.random
-    local replace = string.Replace
+local random = math.random
+local replace = string.Replace
 
-    local MESSAGE_COLOR = Color( 255, 255, 255 )
-    local MESSAGE = "{nick} has rolled a {roll}"
-    local MESSAGE_RANGE = 250
+local MESSAGE_COLOR = Color( 255, 255, 255 )
+local MESSAGE = "{nick} has rolled a {roll}"
+local MESSAGE_RANGE = 250
 
-    local isValid =  commandsLib.register( "roll", {
-        ["prefix"] = { "/", "!" },
-        ["execute"] = function( requester, options )
-            local roll = random( 1, 100 )
+local isValid =  commandsLib.register( "roll", {
+    ["prefix"] = { "/", "!" },
+    ["execute"] = function( requester, options )
+        if ( not DarkRP ) then return end
 
-            local msg = MESSAGE
-            msg = replace( msg, "{nick}", requester:Nick() )
-            msg = replace( msg, "{roll}", roll )
-            
-            if GAMEMODE.Config.alltalk then
-                for _, target in ipairs( player.GetAll() ) do
-                    DarkRP.talkToPerson( target, MESSAGE_COLOR, msg )
-                end
-            else
-                DarkRP.talkToRange( requester, msg, "", MESSAGE_RANGE )
+        local roll = random( 1, 100 )
+
+        local msg = MESSAGE
+        msg = replace( msg, "{nick}", requester:Nick() )
+        msg = replace( msg, "{roll}", roll )
+        
+        if GAMEMODE.Config.alltalk then
+            for _, target in ipairs( player.GetAll() ) do
+                DarkRP.talkToPerson( target, MESSAGE_COLOR, msg )
             end
+        else
+            DarkRP.talkToRange( requester, msg, "", MESSAGE_RANGE )
+        end
 
-        end,
-        ["showCommand"] = false
-    } )
+    end,
+    ["showCommand"] = false
+} )
 
-    if ( isValid ) then
-        print( "Congratulations, you have created 'roll' chat command!" )
-    else
-        print( "Error, a command does already have this name !" )
-    end
+if ( isValid ) then
+    print( "Congratulations, you have created 'roll' chat command!" )
 else
-    error( "You must have DarkRP gamemode" )
+    print( "Error, a command does already have this name !" )
 end
